45a4f8130d59ac2f278ed3b8b1b11bdb2fe121c4
MandavkarPranjal <programmer.pranjal@gmail.com>
2025-07-15 12:31:03 +0530

    fixed long time error

--- home/.config/nvim/lua/lostheaven/plugins/present.lua
+++ home/.config/nvim/lua/lostheaven/plugins/present.lua
@@ -1,1 +1,10 @@
-require("lostheaven.custom.present").setup()
+return {
+	{
+		"present",
+		dir = vim.fn.stdpath("config") .. "/lua/lostheaven/custom",
+		name = "present",
+		config = function()
+			require("lostheaven.custom.present").setup()
+		end,
+	},
+}
